7fb2cbf5565f17e816e19d6dd0785e3f27f4567d
Darsh <darshankondhare2698@gmail.com>
2022-11-30 16:57:43 +0530

    [darsh] Add: check Pattern

---
+++ ProgrammingWithRegex/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace ProgrammingWithRegex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Program with Regex Statements");
+            Console.WriteLine("Select Below Mention Options");
+            bool choice = true;
+            while (choice)
+            {
+                Console.WriteLine("1. Check Pattern to validate Postal Code\n2. Exit");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        PostalCode code = new PostalCode();
+                        code.TestPinCode("400088");
+                        break;
+                    default:
+                        choice = false;
+                        Console.WriteLine("Try Again");
+                        break;
+                }
+            }
+        }
+    }
+}
